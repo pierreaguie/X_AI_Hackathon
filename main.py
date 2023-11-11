a8ba0c6a37a8a6dbeb9c42b2ec82da29fc780efe
pierreaguie <pierre.aguie@gmail.com>
2023-11-11 11:26:50 +0100

    idk

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from models import *
 from data import *
 from train_ViT import *
+from parameters import *
 
 if __name__ == "__main__":
-    print()
+
+    if act == "train":
+        
+        dataModule = DataModule(train_path,
+                        unlabelled_path,
+                        transform_augmentation,
+                        transform,
+                        batch_size,
+                        num_workers)
+
+        val_loader_list = dataModule.val_dataloader_list()
