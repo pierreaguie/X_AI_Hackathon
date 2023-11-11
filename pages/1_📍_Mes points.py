f6967ae94672bd42a56000b82abdb6c6909d01a4
pierreaguie <pierre.aguie@gmail.com>
2023-11-12 00:16:40 +0100

    model + app

--- pages/1_📍_Mes points.py
+++ pages/1_📍_Mes points.py
@@ -6,3 +6,23 @@ st.title("📍 Mes points")
 data = pd.DataFrame(columns = ["Nom", "Longitude", "Latitude", "Image", "État"])
 
 st.dataframe(data)
+
+
+list_coords = []
+
+def submit(nom, long, lat):
+    list_coords.append((long,lat))
+
+st.write("---")
+st.write("# Ajouter un lieu de surveillance")
+with st.form("Lieux", clear_on_submit=True):
+    col1, col2 = st.columns(2)
+    col = st.columns(1)[0]
+    nom = col.text_input("Nom")
+    long = col1.number_input("Latitude")
+    lat = col2.number_input("Longitude")
+    valid_status = st.form_submit_button("Valider", on_click=submit(nom, long, lat))
+    if valid_status:
+        st.success("Point ajouté")
+        data.loc[len(data)] = [nom, long, lat, "", "Pas de fuite"]
+        st.dataframe(data)
