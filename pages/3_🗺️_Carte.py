90bbc0c9fad24ef44ed7206dc0e8810534dfeef2
louis-collomb <150327994+louis-collomb@users.noreply.github.com>
2023-11-12 09:37:31 +0100

    Update 3_🗺️_Carte.py

--- pages/3_🗺️_Carte.py
+++ pages/3_🗺️_Carte.py
@@ -30,3 +30,40 @@ for item in data_list:
 
 # call to render Folium map in Streamlit
 st_data = folium_static(m, width=725)
+
+
+
+
+def recherche(id):
+    for item in data_list:
+        if str(item['id_coord']) == "id_" + id:
+            col1 = ["Id", "Lattitude", "Longitude", "Plume", "X", "Y", "Dernière date"]
+            date = str(item['date'])
+            date = date[6:8] + "/" + date[4:6] + "/" + date[0:4]
+            col2 = [item['id_coord'], item['lat'], item['lon'], item['plume'], item['coord_x'], item['coord_y'], date]
+            table = pd.DataFrame({"Infos": col1, "Valeurs": col2})
+            st.table(table)
+            path = ""
+            if item['plume']:
+                path += "data/images/plume/"
+            else:
+                path += "data/images/no_plume/"
+            path += str(item['date'])+"_methane_mixing_ratio_"+item['id_coord'] +  ".tif"
+            original_image = Image.open(path)
+            #img = load_img(original_image, target_size = (64,64), color_mode = "grayscale")
+            plt.imshow(original_image, cmap='gray')
+            plt.axis('off')
+            st.set_option('deprecation.showPyplotGlobalUse', False)
+            st.pyplot()
+            return
+    if id !="":
+        st.warning("Id inexistant")
+    return
+
+st.markdown("<h1 style='text-align: center'>Rechercher un point</h1>", unsafe_allow_html=True)
+with st.form("Lieux", clear_on_submit=True):
+    name = st.text_input("Id du point recherché :")
+    valid_status = st.form_submit_button("Rechercher", on_click=recherche(name))
+    if valid_status:
+        if name == "":
+            st.warning("Rentrer un id")
