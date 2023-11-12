e1cbc8e474a9efd210a846921692b81ca58a378f
Darthkiller2Jr <d.desaintguilhem@gmail.com>
2023-11-12 13:36:16 +0100

    modified

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # X_AI_Hackathon
 
 Repository for the hackathon team #6 "Code with the bros"
+
--- paths.py
+++
@@ -1,3 +0,0 @@
-train_path = "path/to/train/folder"
-unlabelled_path = "path/to/unlabelled/folder"
-test_path = "path/to/test/folder"
